ba3cffdd4a721dadeeeaab2fc21aab8c5909cac0
Sunny Xu <xusn@uci.edu>
2017-04-14 16:25:28 -0700

    registration looks like it's holding for now, gonna attempt hasing

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,18 +1,33 @@
-$('#register-submit').on('click', function(event) {
-  //TODO Verify for password re-enter
-  if (true) {
-    console.log("CLICK!");
-    var fullname = $('#reg-name').val();
-    var username = $('#reg-user').val();
-    var emailaddr = $('#reg-email').val();
-    var password = $('#reg-pass').val();
-    $.post('http://localhost:3000/user', {
-      name: fullname,
-      user: username,
-      email: emailaddr,
-      pass: password
-    }, function(response) {
-      document.location.href = './index.html';
-    });
-  } else {}
+//Registration submit
+$('#register-form').on('submit', function(event) {
+  if ($('#reg-pass').val() != $('#reg-repass').val()) {
+    event.preventDefault();
+   alert('Different password inputted in fields!');
+ }
+ else {
+   var fullname = $('#reg-name').val();
+   var username = $('#reg-user').val();
+   var emailaddr = $('#reg-email').val();
+   var password = $('#reg-pass').val();
+   $.post('http://localhost:3000/user', {
+     name: fullname,
+     user: username,
+     email: emailaddr,
+     pass: password
+   }, function(response) {
+     document.location.href = './index.html';
+   });
+ }
 });
+
+$('#login-form').on('submit', function(event) {
+  var username = $('#log-name').val();
+  var password = $('#log-pass').val();
+  $.get('http//localhost:3000/user', {
+    user: username, pass: password
+  }, function(response) {
+
+  });
+});
+
+//Helper Functions
--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,33 @@ var mongoose = require('mongoose');
 var UserModel = require('../models/user');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.json([]);
-});
+/*Post for registration form*/
 router.post('/', function(req, res) {
   var body = req.body;
-  var newUser = new UserModel(body);
+  var hashedpassword = req.body.pass; //temp
+  var newUser = new UserModel({
+    user: body.user,
+    pass: body.pass,
+    email: body.email,
+    name: body.name
+  });
   newUser.save(function(err, doc) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      res.json(doc);
+    }
+  });
+});
+/*Post for login*/
+router.post('/login', function(req, res) {
+  var body = req.body;
+  var hashedpassword = req.body.pass; //temp
+  UserModel.find({
+    user: body.user,
+    pass: hashedpassword
+  }, function(err, doc) {
     if (err) {
       console.log(err);
       res.send(err);
